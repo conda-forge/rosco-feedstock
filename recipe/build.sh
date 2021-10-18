a616fcec0e31955c4431d8b86395ab5003949ec2
Nikhar Abbas <nikhar.abbas@colorado.edu>
2021-10-18 13:47:01 -0600

    Update for oneROSCO2
    a different import
    
    cleanup python deps
    
    python as run req
    
    import rosco
    
    rename to ROSCO toolbox
    
    try updating again...
    
    linting fix
    
    remove specific python build variants
    
    updatee for simplicity
    
    make build dir
    
    another .bat attempt
    
    ROSCO path
    
    linting
    
    chdir to rosco
    
    use make call
    
    another try at getting hte right build dir
    
    paths again
    
    Flag cleanup and typo fixes
    
    add libpython on windows
    
    install instead of develop
    
    fix windows install lib test path
    
    change delimeter

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,5 @@ python setup.py install
 
 mkdir ROSCO/build
 cd ROSCO/build 
-
-cmake \
-    -DCMAKE_Fortran_FLAGS="-ffree-line-length-0" \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=Release \
-    ..
-
-make -j${CPU_COUNT} install
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+make install
